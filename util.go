7c21ab2f9df26209b3d7ac26ed3b4dafc98bf1ba
Michael Fogleman <fogleman@gmail.com>
2016-02-27 23:00:37 -0500

    initial commit

---
+++ util.go
@@ -0,0 +1,33 @@
+package imview
+
+import (
+	"image"
+	"image/draw"
+	"image/png"
+	"os"
+)
+
+func LoadImage(path string) (image.Image, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	im, _, err := image.Decode(file)
+	return im, err
+}
+
+func LoadPNG(path string) (image.Image, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return png.Decode(file)
+}
+
+func ImageToRGBA(src image.Image) *image.RGBA {
+	dst := image.NewRGBA(src.Bounds())
+	draw.Draw(dst, dst.Rect, src, image.ZP, draw.Src)
+	return dst
+}
